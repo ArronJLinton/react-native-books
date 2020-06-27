262c9c194d4982cfd3570a8b312d6155374d79f1
Arron J Linton <alinton@2u.com>
2020-06-27 18:18:46 -0500

    working scroll animation

--- screens/BookDetail/index.js
+++ screens/BookDetail/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Image, Linking, View, ScrollView} from 'react-native';
+import React,{useRef} from 'react';
+import { Image, Linking, View, ScrollView, Animated, Dimensions} from 'react-native';
 import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
 
 const BookDetail = ({ route, navigation }) => {
   const { title, authors, imageLinks, previewLink, description } = route.params.data.volumeInfo;
   const { webReaderLink } = route.params.data.accessInfo;
+  const H_MAX_HEIGHT = 200;
+  const H_MIN_HEIGHT = 52;
+  const H_SCROLL_DISTANCE = H_MAX_HEIGHT - H_MIN_HEIGHT;
+  const scrollOffsetY = useRef(new Animated.Value(0)).current;
+  const headerScrollHeight = scrollOffsetY.interpolate({
+    inputRange: [0, H_SCROLL_DISTANCE],
+    outputRange: [H_MAX_HEIGHT, H_MIN_HEIGHT],
+    extrapolate: "clamp"
+  });
+
     return (
       <Container>
         <ScrollView contentContainerStyle={{ justifyContent: 'center', alignItems: 'center'}}>
           {/* <Content> */}
             {/* <Card style={{flex: 0}}> */}
-              <CardItem style={{backgroundColor: 'lightgray', width: '100%', justifyContent: 'center'}}>
-                <Image source={{uri: imageLinks.thumbnail}} resizeMode='contain' style={{height: 300, width: 200}}/> 
-             
-              </CardItem>
+              
               <CardItem>
                 <Body style={{alignItems: 'center'}}>
                   <Text>{title}</Text>
@@ -53,6 +60,28 @@ const BookDetail = ({ route, navigation }) => {
             {/* </Card> */}
           {/* </Content> */}
         </ScrollView>
+        <Animated.View
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          height: headerScrollHeight,
+          width: "100%",
+          overflow: "hidden",
+          zIndex: 999,
+          // STYLE
+          borderBottomColor: "#EFEFF4",
+          borderBottomWidth: 2,
+          padding: 10,
+          backgroundColor: "blue"
+        }}
+        >
+        {/* <CardItem style={{backgroundColor: 'lightgray', width: '100%', justifyContent: 'center'}}> */}
+                <Image source={{uri: imageLinks.thumbnail}} style={{flex: 1}} resizeMode='contain'/> 
+             
+              {/* </CardItem> */}
+      </Animated.View>
       </Container>
     );
   }
--- screens/BookNotes/index.js
+++ screens/BookNotes/index.js
@@ -1,23 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
-import { Container, Body, CardItem, Icon, Button, Header, Content, Tab, Tabs  } from 'native-base';
-import { ScrollView } from 'react-native-gesture-handler';
-import axios from 'axios';
-import { WebView } from 'react-native-webview';
-import { withGlobalContext } from '../../utils/GlobalContext';
-import { SavedBookCard } from '../../components/BookCard';
-import API from '../../utils/API';
+import React, {useEffect, useState, useRef} from 'react';
+import { StyleSheet, Text, View, Image, Dimensions, ScrollView, Animated } from 'react-native';
+import { Container, Content, Tab, Tabs } from 'native-base';
 import HTML from 'react-native-render-html';
+import { withGlobalContext } from '../../utils/GlobalContext';
+import API from '../../utils/API';
 
  const BookNotes = ({ global, route }) => {
   // const { books } = global;
   const { title, Author, bookCover, googleId } = route.params.data;
   const [book, setBook] = useState({});
-  
+  const H_MAX_HEIGHT = 150;
+  const H_MIN_HEIGHT = 52;
+  const H_SCROLL_DISTANCE = H_MAX_HEIGHT - H_MIN_HEIGHT;
+  const scrollOffsetY = useRef(new Animated.Value(0)).current;
+  const headerScrollHeight = scrollOffsetY.interpolate({
+    inputRange: [0, H_SCROLL_DISTANCE],
+    outputRange: [H_MAX_HEIGHT, H_MIN_HEIGHT],
+    extrapolate: "clamp"
+  });
+
   const findBookId = () => {
     API.findById(googleId)
       .then(({data}) => {
-        // console.log('DATA: ', data.webReaderLink)
         setBook(data.volumeInfo)
       })
       .catch(err => console.log(err))
@@ -26,36 +30,60 @@ import HTML from 'react-native-render-html';
   useEffect(() => {
     findBookId();
   }, [])
-
   return (
-    <Container>
-      {/* <ScrollView contentContainerStyle={{ justifyContent: 'center', alignItems: 'center'}}> */}
-        {/* <Content> */}
-          {/* <Card style={{flex: 0}}> */}
-            <CardItem style={{backgroundColor: 'lightgray', width: '100%', justifyContent: 'center'}}>
-              <Image source={{uri: bookCover}} resizeMode='contain' style={{height: 300, width: 200}}/> 
-            </CardItem>
-        <Tabs>
+    <View style={styles.container}> 
+
+        <Tabs style={{ paddingTop: H_MAX_HEIGHT }}>
           <Tab heading="Chapter Notes">
+          <ScrollView
+            onScroll={Animated.event([
+              { nativeEvent: { contentOffset: { y: scrollOffsetY } } }
+            ])}
+            scrollEventThrottle={16}
+          >
             <AccordionExample />
+            </ScrollView>
           </Tab>
           <Tab heading="Description">            
-            <ScrollView style={{ flex: 1 }}>
+          <ScrollView
+            onScroll={Animated.event([
+              { nativeEvent: { contentOffset: { y: scrollOffsetY } } }
+            ])}
+            scrollEventThrottle={16}
+          >
                 <HTML html={book.description} imagesMaxWidth={Dimensions.get('window').width} />
             </ScrollView>
           </Tab>
           <Tab heading="Book Detail">
             <Content>
               <Text>{title}</Text>
-              
               <Text>{Author.fullName}</Text>
               <Text>{book.subtitle}</Text>
               <Text>Pages: {book.pageCount}</Text>
-              <Text>{book.categories[0]}</Text>
+              {/* <Text>{book.categories[0]}</Text> */}
             </Content>
           </Tab>
         </Tabs>
-    </Container>
+      <Animated.View
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          height: headerScrollHeight,
+          width: "100%",
+          overflow: "hidden",
+          zIndex: 999,
+          // STYLE
+          borderBottomColor: "#EFEFF4",
+          borderBottomWidth: 2,
+          padding: 10,
+          backgroundColor: "lightgray"
+        }}
+        >
+        <Image source={{uri: bookCover}} resizeMode='contain' style={{flex: 1}}/> 
+      </Animated.View>
+    </View>
   );
 }
 
